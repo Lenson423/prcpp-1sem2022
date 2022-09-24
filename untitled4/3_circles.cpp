3f1ff36c5cb3e0baa303078bc36ce561d2692b26
Eugene <lenson.eab@gmail.com>
2022-09-24 13:57:20 +0300

    nothing1

--- untitled4/3_circles.cpp
+++ untitled4/3_circles.cpp
@@ -18,16 +18,21 @@ int main() {
     double x1, y1, R1;
     cin >> x1 >> y1 >> R1;
 
+    if (R1 <= 0 or R0 <= 0){
+        cout << "Радиус не может быть отрицательным";
+        return 0;
+    }
+
     double distance = pow((x1 - x0) * (x1 - x0) + (y1 - y0) * (y1 - y0), 0.5);
     if (distance <= epsilon and fabs(R1 - R0) <= epsilon){
         cout << "Окружности совпадают.";
     }else if (distance - R0 - R1 >= epsilon or distance - fabs(R0-R1) <= epsilon){
-        cout << "Окружности не касаются друг-друга";
-    }else{
+        cout << "Окружности не касаются друг друга";
+    }else{ //смотреть приложеный рисунок
         double OP = (R0 * R0 - R1 * R1 + distance * distance) / (2 * distance);
-        double height = pow(R0 * R0 - OP * OP, 0.5);
-        double Ox_P = x0 + OP * (x1 - x0) / distance;
-        double Oy_P = y0 + OP * (y1 - y0) / distance;
+        double height = pow(R0 * R0 - OP * OP, 0.5); // PB и PA на рисунке
+        double Ox_P = x0 + OP * (x1 - x0) / distance; // координата проекции Р на ось абсцисс
+        double Oy_P = y0 + OP * (y1 - y0) / distance; // координата проекции Р на ось ординат
         double deltaForX = height * (y1 - y0) / distance;
         double deltaForY = height * (x1 - x0) / distance;
 
