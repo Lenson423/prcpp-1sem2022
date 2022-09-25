221266ba44bb6905b37b035fc910189b08c6dcf3
Eugene <lenson.eab@gmail.com>
2022-09-25 14:31:47 +0300

    nothing1

--- untitled4/1_TV_programe.cpp
+++ untitled4/1_TV_programe.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include "windows.h"
 
-using std::cout, std::cin, std::endl, std::string;
+using std::cout, std::cin, std::endl, std::string, std::getline;
 
 int main() {
     SetConsoleOutputCP(CP_UTF8);
@@ -13,24 +13,37 @@ int main() {
 
     cout << "Последовательно введите название, корректные время начала(сперва часы, затем минуты) и продолжительность программы "
             "№1(в минутах)" << endl;
-    cin >> firstName;
+    getline(cin, firstName);
     cin >> startTimeH1;
     cin >> startTimeM1;
     cin >> longest1;
 
     cout << "Последовательно введите название, корректные время начала(сперва часы, затем минуты) и продолжительность программы "
             "№2(в минутах)" << endl;
-    cin >> secondName;
+    cin.ignore();
+    getline(cin, secondName);
     cin >> startTimeH2;
     cin >> startTimeM2;
     cin >> longest2;
 
     cout << "Последовательно введите название, корректные время начала(сперва часы, затем минуты) и продолжительность программы "
             "№3(в минутах)" << endl;
-    cin >> thirdName;
+    cin.ignore();
+    getline(cin, thirdName);
     cin >> startTimeH3;
     cin >> startTimeM3;
     cin >> longest3;
+
+    if (startTimeH1 > 24 or startTimeH1 < 0 or startTimeH2 > 24 or startTimeH2 < 0 or startTimeH3 < 0 or startTimeH3 > 24){
+        cout << "Количество часов введено неверно.";
+        return 0;
+    }
+
+    if (startTimeM1 > 59 or startTimeH1 < 0 or startTimeH2 > 59 or startTimeH2 < 0 or startTimeH3 < 0 or startTimeH3 > 59){
+        cout << "Количество минут введено неверно.";
+        return 0;
+    }
+
 
     unsigned int startTime1 = startTimeH1 * 60 + startTimeM1;
     unsigned int startTime2 = startTimeH2 * 60 + startTimeM2;
@@ -40,14 +53,25 @@ int main() {
     int hours, minuts;
     cin >> hours >> minuts;
     int currentTime = hours * 60 + minuts;
+    int index = 0;
     if (currentTime <= startTime1 + longest1 and currentTime >= startTime1){
-        cout << "Андрей смотрит '" << firstName << "'";
-    }else if (currentTime <= startTime2 + longest2 and currentTime >= startTime2){
-        cout << "Андрей смотрит '" << firstName << "'";
-    }else if (currentTime <= startTime3 + longest3 and currentTime >= startTime3){
-        cout << "Андрей смотрит '" << firstName << "'";
-    }else{
+        cout << "Андрей может смотреть '" << firstName << "'" << endl;
+        ++index;
+    }
+    if (currentTime <= startTime2 + longest2 and currentTime >= startTime2){
+        cout << "Андрей может смотреть '" << secondName << "'" << endl;
+        ++index;
+    }
+    if (currentTime <= startTime3 + longest3 and currentTime >= startTime3){
+        cout << "Андрей может смотреть  '" << thirdName << "'" << endl;
+        ++index;
+    }
+    if (index == 0){
         cout << "В данный момент не идет никакая программа";
+    }else if (index == 1){
+        cout << "В данный момент идет 1 программа";
+    }else{
+        cout << "В данный момент идут " << index << " программы";
     }
     return 0;
 }
--- untitled4/6_yesterday(with functions).cpp
+++ untitled4/6_yesterday(with functions).cpp
@@ -1,17 +1,14 @@
-//
-// Created by lenso on 24.09.2022.
-//
 #include <iostream>
 #include "windows.h"
 
 using std::cout, std::cin, std::endl;
 
-int main() {
-    SetConsoleOutputCP(CP_UTF8);
-    cout << "Последовательно введите номера текущих дня, месяца и года (нашей эры):" << endl;
-    unsigned int day, month, year;
-    cin >> day >> month >> year;
+int day_error(){
+    cout << "Введенного дня в данном месяце не существует";
+    exit(0);
+}
 
+int analys(int day, int month, int year) {
     if (month > 12 or year < 1){
         cout << "Введенного месяца или года не существует" << endl;
         exit(0);
@@ -25,8 +22,7 @@ int main() {
         case 10:
         case 12:
             if (day > 31) {
-                cout << "Введенного дня в данном месяце не существует";
-                exit(0);
+                day_error();
             }
             break;
         case 4:
@@ -34,53 +30,58 @@ int main() {
         case 9:
         case 11:
             if (day > 30) {
-                cout << "Введенного дня в данном месяце не существует";
-                exit(0);
+                day_error();
             }
             break;
         case 2:
             if (year % 4 != 0 or (year % 100 == 0 and year % 400 != 0)) {
                 if (day > 28){
-                    cout << "Введенного дня в данном месяце не существует";
-                    exit(0);
+                    day_error();
                 }
             }else {
                 if (day > 29){
-                    cout << "Введенного дня в данном месяце не существует";
-                    exit(0);
+                    day_error();
                 }
             }
             break;
     }
+    return 0;
+}
 
+int main() {
+    SetConsoleOutputCP(CP_UTF8);
+    cout << "Последовательно введите номера текущих дня, месяца и года (нашей эры):" << endl;
+    unsigned int day, month, year;
+    cin >> day >> month >> year;
+    analys (day, month, year);
     if (day == 1){
         if (month == 1) {
             year -= 1;
             month = 12;
             day = 31;
-        }else {
-            month -= 1;
-            switch (month) {
-                case 1:
-                case 3:
-                case 5:
-                case 7:
-                case 8:
-                case 10:
-                    day = 31;
-                    break;
-                case 4:
-                case 6:
-                case 9:
-                case 11:
-                    day = 30;
-                    break;
-                case 2:
-                    if (year % 4 != 0 or (year % 100 == 0 and year % 400 != 0)) {day = 28;}
-                    else {day = 29;}
-                    break;
-            }
-        }}else{
+    }else {
+        month -= 1;
+        switch (month) {
+            case 1:
+            case 3:
+            case 5:
+            case 7:
+            case 8:
+            case 10:
+                day = 31;
+                break;
+            case 4:
+            case 6:
+            case 9:
+            case 11:
+                day = 30;
+                break;
+            case 2:
+                if (year % 4 != 0 or (year % 100 == 0 and year % 400 != 0)) {day = 28;}
+                else {day = 29;}
+                break;
+        }
+    }}else{
         day -= 1;
     }
 
