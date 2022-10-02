6c7a121e4ab21de292a260b8b15d86f3189f2eaf
Eugene <lenson.eab@gmail.com>
2022-10-02 17:04:13 +0300

    3 laba

--- untitled7/4.cpp
+++ untitled7/4.cpp
@@ -12,42 +12,39 @@ int main() {
     cout << "Введите число r:" << endl;
     int radius;
     cin >> radius;
-
-    //O(n)
-    int result = 0;
-    for(int y = 0; y <= radius; y++)
-        result += int(sqrt(radius * radius - y * y));
-    result = 4 * result + 1;
-    if (result == 1){
-        cout << "1 точка" << endl;
-    }else {
-        cout << result << " точек"<< endl;
+    if (radius < 1){
+        cout << "Радиус не может быть отрицательным или равным 0" ;
+        return 0;
     }
+
+    //O(n) with sqrt
+    int result = 0;
+    for (int y = 0; y <= radius; y++)
+        result += int(sqrt(radius * radius - y * y));
+    result = result + radius + 1;
+    cout << result << " точек" << endl;
+
 
     // 0(n*n)
     int res = 0;
-    for(int i = 0; i <= radius; i++) {
+    for (int i = 0; i <= radius; i++) {
         for (int j = 1; j <= radius; j++) {
             if (i * i + j * j <= radius * radius) {
                 res++;
             }
         }
     }
-    cout << res * 4 + 1 << endl;
+    cout << res + radius + 1 << endl;
 
-    // O(n)
+    // O(n) without sqrt
     int result1 = 0;
     int counter = radius * radius;
-    for (int i = 1; i <= counter; i += 4){
+    for (int i = 1; i <= counter; i += 4) {
         result1 += counter / i;
         result1 -= counter / (i + 2);
     }
-    result1 = 4 * result1 + 1;
-    if (result1 == 1){
-        cout << "1 точка" << endl;
-    }else {
-        cout << result << " точек"<< endl;
-    }
+    result1 = result1 + radius + 1;
+    cout << result1 << " точек" << endl;
 
     return 0;
 }
